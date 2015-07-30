d6418162e3ced243dbc9e6e92ad7d962e55623ec
Mateus Mesquita <n2o.matt@gmail.com>
2015-07-29 22:04:43 -0300

    Update license, info and file headers.

--- Categories/NSString+COWStringExtensions.h
+++ Categories/NSString+COWStringExtensions.h
@@ -1,40 +1,40 @@
 //----------------------------------------------------------------------------//
-//                   _   _ ____   ___  ____                                   //
-//                  | \ | |___ \ / _ \| __ )  ___  _   _ ____                 //
-//                  |  \| | __) | | | |  _ \ / _ \| | | |_  /                 //
-//                  | |\  |/ __/| |_| | |_) | (_) | |_| |/ /                  //
-//                  |_| \_|_____|\___/|____/ \___/ \__, /___|                 //
-//                                                 |___/                      //
+//                 █      █                                                   //
+//                 ████████                                                   //
+//               ██        ██                                                 //
+//              ███  █  █  ███    NSString+COWStringExtensions.h              //
+//              █ █        █ █    UIKitHelpers                                //
+//               ████████████                                                 //
+//             █              █   Copyright (c) 2015 AmazingCow               //
+//            █     █    █     █  www.AmazingCow.com                          //
+//            █     █    █     █                                              //
+//             █              █   N2OMatt - n2omatt@amazingcow.com            //
+//               ████████████     www.amazingcow.com/n2omatt                  //
 //                                                                            //
-//                                   N2OMatt                                  //
-//                             N2OMatt@N2OBoyz.com                            //
-//                           www.N2OBoyz.com/N2OMatt                          //
 //                                                                            //
-//                         Copyright (C) 2015 N2OBoyz.                        //
-//                                                                            //
-//      This software is provided 'as-is', without any express or implied     //
-//    warranty. In no event will the authors be held liable for any damages   //
-//                   arising from the use of this software.                   //
+//                  This software is licensed as LGPL-v3                      //
+//                 CHECK THE COPYING FILE TO MORE DETAILS                     //
 //                                                                            //
 //    Permission is granted to anyone to use this software for any purpose,   //
 //   including commercial applications, and to alter it and redistribute it   //
 //               freely, subject to the following restrictions:               //
 //                                                                            //
+//     0. You **CANNOT** change the type of the license.                      //
 //     1. The origin of this software must not be misrepresented;             //
 //        you must not claim that you wrote the original software.            //
 //     2. If you use this software in a product, an acknowledgment in the     //
 //        product IS HIGHLY APPRECIATED, both in source and binary forms.     //
-//        (See opensource.N2OBoyz.com/acknowledgment.html for details).       //
+//        (See opensource.AmazingCow.com/acknowledgment.html for details).    //
 //        If you will not acknowledge, just send us a email. We'll be         //
 //        *VERY* happy to see our work being used by other people. :)         //
-//        The email is: acknowledgment.opensource@N2OBoyz.com                 //
+//        The email is: acknowledgment.opensource@AmazingCow.com              //
 //     3. Altered source versions must be plainly marked as such,             //
 //        and must notbe misrepresented as being the original software.       //
 //     4. This notice may not be removed or altered from any source           //
 //        distribution.                                                       //
 //     5. Most important, you must have fun. ;)                               //
 //                                                                            //
-//         Visit OpenSource.N2OBoyz.com for more open-source projects.        //
+//      Visit opensource.amazingcow.com for more open-source projects.        //
 //                                                                            //
 //                                  Enjoy :)                                  //
 //----------------------------------------------------------------------------//
